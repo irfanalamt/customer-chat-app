ea1135f371fdb3e642fa98320e017578cf5d7aca
Irfan <irfanalamt@gmail.com>
2021-05-14 18:58:04 -0400

    brushing up

---
+++ src/components/counterComponent.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 1,
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <span>{this.formatCount()}</span>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Haha" : count;
+  }
+}
+
+export default Counter;
