dcf8e30f37c9549808695f86c57bd5d9efce587f
Irfan <irfanalamt@gmail.com>
2021-05-18 12:57:48 -0400

    daily update

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -3,19 +3,21 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 1,
+    tags: ["tag1", "tag2", "tag3"],
   };
 
   render() {
     return (
-      <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
-      </React.Fragment>
+      <div>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>
+              <h1>{tag}</h1>
+            </li>
+          ))}
+        </ul>
+      </div>
     );
-  }
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Haha" : count;
   }
 }
 
