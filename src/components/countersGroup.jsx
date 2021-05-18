aefe93e8a19c3c1798335094f456bff20427c1db
Irfan <irfanalamt@gmail.com>
2021-05-18 18:09:55 -0400

    passing value between components

---
+++ src/components/countersGroup.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Counter from "./counterComponent";
+
+class Counters extends Component {
+  state = {
+    counterValues: [
+      { id: 0, value: 5 },
+      { id: 1, value: 0 },
+      { id: 2, value: 6 },
+      { id: 3, value: 78 },
+    ],
+  };
+  render() {
+    return (
+      <div>
+        {this.state.counterValues.map((counter) => (
+          <Counter key={counter.id} value={counter.value} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
