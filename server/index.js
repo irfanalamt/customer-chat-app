a5402641b5fd3ee411bd51403cddbefb5bc4f109
Irfan <irfanalamt@gmail.com>
2021-05-21 21:32:04 -0400

    added backend

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+io.on("connection", (socket) => {
+  socket.on("message", ({ name }) => {
+    io.emit("message", { name, message });
+  });
+});
+
+http.listen(4000, () => console.log("Listening on port 4000"));
